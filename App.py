347fe473f74b291ce60f926e58ec6fd921f64ffe
kandosa <yingjie.huang2013@gmail.com>
2022-01-23 11:55:49 -0800

    updating html files

--- App.py
+++ App.py
@@ -13,11 +13,11 @@ def index():
         try:
             limit = int(request.form["limit"])
         except ValueError:
-            errors += "<p>invalid input.</p>\n".format(request.form["limit"])
+            errors += "invalid input\n".format(request.form["limit"])
         try:
             protein = int(request.form["protein"])
         except ValueError:
-            errors += "<p>protein needs to be one of .</p>\n".format(request.form["protein"])
+            errors += "protein needs to be from the menu below.\n".format(request.form["protein"])
         if limit is not None and protein is not None:
             try:
                 meal, calorie, recipe = main(limit, protein)
@@ -25,9 +25,9 @@ def index():
                     raise TypeError
                 return render_template("result.html", meal=meal, calorie=calorie, recipe=recipe)
             except TypeError:
-                errors += "<p>higher limit</p>\n".format(request.form["limit"])
+                errors += "No dish under this calories, Please enter a higher limit.\n".format(request.form["limit"])
 
-    return render_template("index.html")
+    return render_template("index.html", errors=errors)
 
 
 if __name__ == "__main__":
